e0bef67f74a7d8f8bf57e2ff2d2a2fc72cdeb510
hangyejiadao <953917332@qq.com>
2017-12-18 17:25:02 +0800

    123

---
+++ 3.4.0/aspnet-core/src/Demo.Web.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Abp.AspNetCore.Mvc.Authorization;
+using Demo.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Web.Controllers
+{
+    [AbpMvcAuthorize]
+    public class HomeController : DemoControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
