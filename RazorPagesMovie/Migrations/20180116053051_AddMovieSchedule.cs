4246f3e8b99be8f32ed5449d3d40b799e8e84a41
hangyejiadao <953917332@qq.com>
2018-01-16 13:32:44 +0800

    123

---
+++ RazorPagesMovie/Migrations/20180116053051_AddMovieSchedule.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace RazorPagesMovie.Migrations
+{
+    public partial class AddMovieSchedule : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Schedule",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PrivateSchedule = table.Column<string>(nullable: true),
+                    PrivateScheduleSize = table.Column<long>(nullable: false),
+                    PublicSchedule = table.Column<string>(nullable: true),
+                    PublicScheduleSize = table.Column<long>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    UploadDT = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedule", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Schedule");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+        }
+    }
+}
