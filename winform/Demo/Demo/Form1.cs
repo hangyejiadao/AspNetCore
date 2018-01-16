61a65e6a645b4152c4d5c3dddf3b443ca12c3247
hangyejiadao <953917332@qq.com>
2018-01-16 12:36:29 +0800

    asd

---
+++ winform/Demo/Demo/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "http://localhost:5000/";
+            string result = HttpHelper.Request(url + "api/Values/", null, null, HttpMethod.GET);
+            Console.WriteLine(result);
+
+            string getid = HttpHelper.Request(url + "api/Values/Get?id=1", null, null, HttpMethod.GET);
+            Console.WriteLine(getid);
+        }
+    }
+}
---
+++ winform/Demo/Demo/HttpHelper.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class HttpHelper
+    {
+        public static string Request(string url, Dictionary<string, dynamic> args, Dictionary<string, dynamic> header = null,
+            HttpMethod method = HttpMethod.POST, string ContentType = "application/json")
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.ContentType = ContentType;
+            request.Method = method.ToString();
+            request.Timeout = 5000;
+            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36";
+            if (header != null)
+            {
+              
+                    foreach (var item in header)
+                    {
+                        request.Headers.Add(item.Key, item.Value);
+                    }
+              
+            }
+            if (args != null)
+            { 
+                string json = JsonConvert.SerializeObject(JsonConvert.SerializeObject(args));
+                byte[] data = Encoding.UTF8.GetBytes(json);
+                request.ContentLength = data.Length;
+                using (Stream writer = request.GetRequestStream())
+                {
+                    writer.Write(data, 0, data.Length);
+                }
+            }
+            else
+            {
+                request.ContentLength = 0;
+            }
+            using (StreamReader reader = new StreamReader(request.GetResponse().GetResponseStream()))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+
+    public enum HttpMethod
+    {
+        POST,
+        PUT,
+        DELETE,
+        GET
+    }
+}
