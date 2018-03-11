d17ad69eaa99d76c4496706d8742d2bcff09a11e
hangyejiadao <953917332@qq.com>
2018-03-11 10:16:49 +0800

    aa

--- 数据保护/数据保护/Program.cs
+++ 数据保护/数据保护/Program.cs
@@ -7,16 +7,40 @@ namespace 数据保护
     {
         static void Main(string[] args)
         {
+            #region A
+
             // add data protection services
+            //var serviceCollection = new ServiceCollection();
+            //serviceCollection.AddDataProtection();
+            //var services = serviceCollection.BuildServiceProvider();
+
+            //// create an instance of MyClass using the service provider
+            //var instance = ActivatorUtilities.CreateInstance<MyClass>(services);
+            //instance.RunSample();
+            //Console.ReadKey();
+
+            #endregion
+
+            #region B
+
             var serviceCollection = new ServiceCollection();
             serviceCollection.AddDataProtection();
             var services = serviceCollection.BuildServiceProvider();
 
-            // create an instance of MyClass using the service provider
-            var instance = ActivatorUtilities.CreateInstance<MyClass>(services);
-            instance.RunSample();
-            Console.ReadKey();
-            
+            //Get an IDataProtector from the IServiceProvider
+            var protector = services.GetDataProtector("Contoso.Example.v2");
+            Console.WriteLine("Enter Input:");
+
+            string input = Console.ReadLine();
+            //protect the payload
+            string protectedPayload = protector.Protect(input);
+            Console.WriteLine($"Protect returned:{protectedPayload}");
+            //unprotect the payload
+            string unprotectedPayload = protector.Unprotect(protectedPayload);
+            Console.WriteLine($"Unprotect returned:{unprotectedPayload}");
+            Console.ReadLine();
+
+            #endregion
         }
     }
 
