d6fbf5e67d0f5b71883f24584aac16817e5ab639
hangyejiadao <953917332@qq.com>
2018-03-11 14:06:39 +0800

    adfadf

---
+++ 内存缓存/内存缓存/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using 内存缓存.Models;
+
+namespace 内存缓存.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IMemoryCache _cache;
+
+        public HomeController(IMemoryCache memoryCache)
+        {
+            _cache = memoryCache;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult CacheTryGetValuesSet()
+        {
+            DateTime cacheEntry;
+            //Look for cache key;
+            if (!_cache.TryGetValue(CacheKeys.Entry, out cacheEntry))
+            {
+                cacheEntry = DateTime.Now;
+                //set cache options
+                var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(3));
+                //save data in cache
+                _cache.Set(CacheKeys.Entry, cacheEntry, cacheEntryOptions);
+            }
+            return View("Cache", cacheEntry);
+        }
+
+        public IActionResult CacheGetOrCreate()
+        {
+            var cacheEntry = _cache.GetOrCreate(CacheKeys.Entry, entry =>
+            {
+                entry.SlidingExpiration = TimeSpan.FromSeconds(3);
+                return DateTime.Now;
+            });
+            return View("Cache", cacheEntry);
+        }
+
+        public async Task<IActionResult> CacheGetOrCreateAsync()
+        {
+            var cacheEntry = await _cache.GetOrCreateAsync(CacheKeys.Entry, entry =>
+            {
+                entry.SlidingExpiration = TimeSpan.FromSeconds(3);
+                return Task.FromResult(DateTime.Now);
+            });
+            return View("Cache", cacheEntry);
+        }
+
+        public IActionResult CacheGet()
+        {
+            var cacheEntry = _cache.Get<DateTime?>(CacheKeys.Entry);
+            return View("Cache", cacheEntry);
+        }
+    }
+}
